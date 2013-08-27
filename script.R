5ee5c435bb6a2c8e45a06614b78e62553b180af1
alexdthomas <asus4@uw.edu>
2013-08-27 13:38:51 -0600

    some more detail

--- script.R
+++ script.R
@@ -14,11 +14,19 @@ mapFile.new[,"TITLE"]<-"Intertidal microbes 16s for 2009 and 2010"
 unique(mapFile.new[,"TITLE"])
 
 #compare old and new versions
+
+#single function solution, not very detailed output
+all.equal(mapFile.old, mapFile.new)
+mapFile.old[,21]
+
+#very verbose
 mapFile.old==mapFile.new
+#note NA values are not equal, return NA, which may also be helpful
 
 #find row and columns where two files do not match
 which(!mapFile.old==mapFile.new, arr.ind=TRUE)
+#but this only returns that changed values
 
 #can bind the old nad new versions together to compare side by side
 cbind(mapFile.old[which(!mapFile.old==mapFile.new, arr.ind=TRUE)],
-			+ mapFile.new[which(!mapFile.old==mapFile.new, arr.ind=TRUE)])
+			mapFile.new[which(!mapFile.old==mapFile.new, arr.ind=TRUE)])
